41093e3d120943598c8da925c690d5e7b1faad07
Nicholas Esposito <nesposi3@oswego.edu>
2018-02-12 19:51:55 -0500

    Rough layout and functionallity idea for the GUI

--- GUISkeleton/src/guiskeleton/GUISkeleton.java
+++ GUISkeleton/src/guiskeleton/GUISkeleton.java
@@ -13,34 +13,48 @@ import javafx.scene.control.Button;
 import javafx.scene.layout.StackPane;
 import javafx.stage.Stage;
 import java.util.ArrayList;
+import javafx.geometry.Pos;
 import javafx.scene.control.TextField;
+import javafx.scene.control.TextArea;
 
 /**
  *
  * @author Nick
  */
 public class GUISkeleton extends Application {
-    ArrayList<String> inOutlist = new ArrayList();
+
     @Override
     public void start(Stage primaryStage) {
-        TextField inOutField = new TextField();
-        
+        ArrayList<String> inOutList = new ArrayList(); //Contains inputs and outputs as strings to be shown to the user 
+        TextField inField = new TextField(); //The 
+        TextArea outField = new TextArea();
+        outField.setMaxSize(300, 400);
+        //outField.setDisable(true); This dissallows the user to input text in the output box, but unfortunately greys it out. If you know  another way to stop the user from doing this, please implement it
+        inField.setMaxWidth(300);
+
         Button btn = new Button();
         btn.setText("Enter");
         btn.setOnAction(new EventHandler<ActionEvent>() {
-            
+
             @Override
             public void handle(ActionEvent event) {
-                System.out.println("Hello Succ!");
+                outField.clear();
+                String s = inField.getText();
+                inOutList.add(s); //All inputs and outputs will be added to the list in the order they were entered and shown to the user in the output field
+                for(String item:inOutList){
+                    outField.appendText(item + "\n");
+                }
             }
         });
-        
+        //These are the allignments to this pane that I have been experimenting with
         StackPane root = new StackPane();
-        
-        root.getChildren().addAll(btn, inOutField);
-        
+        root.getChildren().addAll(btn, inField,outField);
+        root.setAlignment(btn, Pos.BOTTOM_RIGHT);
+        root.setAlignment(inField, Pos.BOTTOM_CENTER);
+        root.setAlignment(outField, Pos.TOP_CENTER);
+
         Scene scene = new Scene(root, 800, 500);
-        
+
         primaryStage.setTitle("GUI");
         primaryStage.setScene(scene);
         primaryStage.show();
@@ -52,5 +66,5 @@ public class GUISkeleton extends Application {
     public static void main(String[] args) {
         launch(args);
     }
-    
+
 }
