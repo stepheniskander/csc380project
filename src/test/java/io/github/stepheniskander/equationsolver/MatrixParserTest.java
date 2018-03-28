33fd0a272b3294d2986969ccd182897f384acdf7
Stephen Iskander <siskande@oswego.edu>
2018-03-28 16:51:24 -0400

    Remove print in MatrixParserTest

--- src/test/java/io/github/stepheniskander/equationsolver/MatrixParserTest.java
+++ src/test/java/io/github/stepheniskander/equationsolver/MatrixParserTest.java
@@ -6,7 +6,7 @@ public class MatrixParserTest extends TestCase {
     public void testMatrixParse() {
         MatrixParser test = new MatrixParser();
         Matrix ex = test.parse("[[1,2,3] [3,4,5]]");
-        System.out.println(ex.toString());
+        // System.out.println(ex.toString());
         assertEquals(ex.toString(),"1.0 2.0 3.0 \n" +
                 "3.0 4.0 5.0");
     }
