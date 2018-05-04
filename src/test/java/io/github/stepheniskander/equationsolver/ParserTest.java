5784e97e77cbf105aba9376cc48a98a2130630ea
Stephen Iskander <siskande@oswego.edu>
2018-05-03 20:41:33 -0400

    update matrixParse test

--- src/test/java/io/github/stepheniskander/equationsolver/ParserTest.java
+++ src/test/java/io/github/stepheniskander/equationsolver/ParserTest.java
@@ -9,9 +9,7 @@ public class ParserTest {
     @Test
     public void testMatrixParse() {
         Matrix ex = Parser.parseMatrix("[[1,2,3] [3,4,5]]");
-        System.out.println(ex.toString());
-        assertEquals(ex.toString(),"1 2 3 \n" +
-                "3 4 5");
+        assertEquals(ex.toString(), "[[1,2,3] [3,4,5]]");
     }
 
     @Test
